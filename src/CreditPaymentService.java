9a0ff034ca344a28d91db0c3faa8c383f2af589f
Neto test <lsorlova82@gmail.com>
2022-12-04 10:01:28 +0200

    right service formula

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,10 @@
 public class CreditPaymentService {
     public double calculate(int credit, int term, double percent){
-        int time = term*12;
-        double payment = credit * ((percent / 12) / (1 - (1 / (1 + 9.99 / 12))));
-        return payment;
+        int time = term * 12; // срок кредита в месяцах
+        double monthlyPercent = percent / 12 / 100; //месячная процентная ставка
+        double a = 1 + monthlyPercent; // выражение под степенью
+        double payment = credit * ((monthlyPercent * Math.pow(a,time)) / (Math.pow(a,time)-1));
+        int YourPayment = (int) payment; // месячный платеж
+        return YourPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int credit = 1_000_000;
-        int term = 1;
-        double percent = 9.99;
-        double payment = service.calculate(credit, term, percent);
-        System.out.println( "Your monthly payment is " + payment);
+        //int credit = 1_000_000; сумма кредита
+        //int term = 1; срок кредита в годах
+        //double percent = 9.99; годовая процентная ставка
+        //double Payment = service.calculate(credit, term, percent);
+        //int YourPayment = (int) payment;
 
+        System.out.println( "Credit term 1 year. Your monthly payment is ");
+        System.out.println(service.calculate(1_000_000,1,9.99));
+
+        System.out.println( "Credit term 2 years. Your monthly payment is ");
+        System.out.println(service.calculate(1_000_000,2,9.99));
+
+
+        System.out.println( "Credit term 3 years. Your monthly payment is ");
+        System.out.println(service.calculate(1_000_000,3,9.99));
     }
 
 }
